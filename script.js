95a5c123a2a6bd6e34871f555e97d760a681b9bc
Abhisindh Chatterjee <abhisindh123@gmail.com>
2021-07-12 12:15:30 +0530

    new shirt

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 var selectedSet=[];
 var selectedSetName='';
-debugArray=[];
 function reset(){
     if (confirm('Are you sure you want to reset the order of list items?')){
-        var setOne=['IMG_20210709_200308.jpg', 'IMG_20210709_200357.jpg', 'IMG_20210709_200340.jpg', 'IMG_20210709_200408.jpg', 'IMG_20210709_200419.jpg', 'IMG_20210709_200432.jpg', 'IMG_20210709_200445.jpg', 'IMG_20210709_200454.jpg', 'IMG_20210709_200501.jpg', 'IMG_20210709_200512.jpg', 'IMG_20210709_200521.jpg', 'IMG_20210709_200536.jpg', 'IMG_20210709_200545.jpg', 'IMG_20210709_200552.jpg', 'IMG_20210709_200600.jpg', 'IMG_20210709_200648.jpg', 'IMG_20210709_200832.jpg', 'IMG_20210709_200913.jpg']
+        var setOne=['IMG_20210709_200308.jpg', 'IMG_20210709_200357.jpg', 'IMG_20210709_200340.jpg', 'IMG_20210709_200408.jpg', 'IMG_20210709_200419.jpg', 'IMG_20210709_200432.jpg', 'IMG_20210709_200445.jpg', 'IMG_20210709_200454.jpg', 'IMG_20210709_200501.jpg', 'IMG_20210709_200512.jpg', 'IMG_20210709_200521.jpg', 'IMG_20210709_200536.jpg', 'IMG_20210709_200545.jpg', 'IMG_20210709_200552.jpg', 'IMG_20210709_200600.jpg', 'IMG_20210709_200648.jpg', 'IMG_20210709_200832.jpg', 'IMG_20210709_200913.jpg','IMG_20210712_092905.jpg']
     var setTwo=['a','b','c','d','e','f','g','h']
     var setThree=['1','2','3','4','5','6']
     localStorage.setItem('setOne',JSON.stringify(setOne));
@@ -27,12 +26,7 @@ function selectSet(set){
 function selectItem(){
     selection=selectionAlgorithm();
     
-    console.log(selection);
-    var lastOccur=debugArray.lastIndexOf(selection);
-    debugArray.push(selection);
-    if (lastOccur!=-1){
-        console.log(debugArray.length-lastOccur);
-    }
+    
     
 
     const index=selectedSet.indexOf(selection);
@@ -49,6 +43,6 @@ function addImage(x){
 function selectionAlgorithm(){
     var len=selectedSet.length;
     var x=Math.random();
-    console.log(x);
+
     return selectedSet[Math.floor(((len*0.9)**(x))-1)];
 }
